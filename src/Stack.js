7197d47c586e45287fa6fe2a7cc595d36319f474
hojat <hojat72elect@gmail.com>
2023-05-31 00:04:09 -0400

    Added JSDoc

--- src/Stack.js
+++ src/Stack.js
@@ -1,3 +1,8 @@
+/**
+ * Right now, you can only initialize an empty Stack.
+ * In this implementation I have used an Array.
+ * Our stacks currently support number, string, and also object.
+ */
 class Stack {
 
     constructor() {
@@ -5,11 +10,19 @@ class Stack {
         this.length = 0;
     }
 
+    /**
+     *
+     * @param {number|string|Object} element
+     */
     push(element) {
         this._dataStore.push(element);
         this.length++;
     }
 
+    /**
+     *
+     * @returns {number|string|Object}
+     */
     pop() {
         if (this.length === 0)
             throw new Error("An empty stack can't be popped!");
@@ -17,6 +30,10 @@ class Stack {
         return this._dataStore.pop();
     }
 
+    /**
+     *
+     * @returns {number|string|Object}
+     */
     peek() {
         if (this.length === 0)
             return "The stack is empty";
@@ -25,7 +42,7 @@ class Stack {
 
     clear() {
         this._dataStore = [];
-        this.length=0;
+        this.length = 0;
     }
 }
 
