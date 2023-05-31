2a480f02307c73cec2e932cf4ab67bcfdce922b2
hojat <hojat72elect@gmail.com>
2023-05-31 13:19:31 -0400

    Fully migrated Queue.js to flow

--- src/Queue.js
+++ src/Queue.js
@@ -1,56 +1,58 @@
+// @flow
 class Queue {
+
+    _dataStore: Array<any>;
 
     constructor() {
         this._dataStore = [];
     }
 
     /**
-     * user can only read the length of Queue, can't write to it.
-     *
-     * @returns {number}
+     * User can only read the length of Queue, can't write to it.
      */
-    get length() {
+    get length(): number {
         return this._dataStore.length;
     }
 
     /**
-     *
      * @param element will be added at the end of the queue.
      */
-    enqueue(element) {
+    enqueue(element: any) {
         this._dataStore.push(element);
     }
 
     /**
-     *
-     * the first element of the queue (the oldest one) will be
+     * @returns the first element of the queue (the oldest one) will be
      * returned and removed from queue.
      */
-    dequeue() {
+    dequeue(): any {
         if (this._dataStore.length === 0)
             throw new Error("The queue is empty!");
         return this._dataStore.shift();
     }
 
     /**
-     * just peeking to see what is the first element in line.
+     * @returns Just peeking to see what is the first element in line.
      */
-    peekFront() {
+    peekFront(): any {
         if (this._dataStore.length === 0)
             return "The queue is empty";
         return this._dataStore[0];
     }
 
     /**
-     * Just peeking to see what is the last element in this line.
+     * @returns Just peeking to see what is the last element in this line.
      */
-    peekBack() {
+    peekBack(): any {
         if (this._dataStore.length === 0)
             return "The queue is empty";
         return this._dataStore[this._dataStore.length - 1];
     }
 
-    toString() {
+    /**
+     * @returns string representation of the Queue.
+     */
+    toString(): string {
         let retStr = "";
         for (let i = 0; i < this._dataStore.length; ++i) {
             retStr += this._dataStore[i] + "--"
