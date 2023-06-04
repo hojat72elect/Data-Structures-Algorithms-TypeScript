901d5d6cb20601abe9638b88319bcfb181bc942d
hojat <hojat72elect@gmail.com>
2023-06-04 01:35:24 -0400

    initial migration of Dictionary to flow JS

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,25 +1,26 @@
+// @flow
 /**
  * A Key-Value pair database that's supposed to be a better version
  * of JS Objects.
  *
  * @type {Dictionary|{}}
  */
-class Dictionary {
-    _dataStore;
+class Dictionary<K, V> {
+    _dataStore:Object;
 
     constructor() {
         this._dataStore = [];
     }
 
-    add(key, value) {
+    add(key:any, value:V) {
         this._dataStore[key] = value;
     }
 
-    find(key) {
+    find(key:any):V {
         return this._dataStore[key];
     }
 
-    remove(key) {
+    remove(key:any) {
         delete this._dataStore[key];
     }
 
@@ -27,24 +28,24 @@ class Dictionary {
      *  The shown result is already sorted according to keys (incrementally).
      * @returns {string}
      */
-    showAll() {
+    showAll():string {
         let retStr = "";
-        (Object.keys(this._dataStore).sort()).forEach((key) => {
+        (Object.keys(this._dataStore).sort()).forEach((key:string) => {
             retStr += `* ${key} -> ${this._dataStore[key]} *`;
         })
         return retStr;
     }
 
-    count() {
+    count():number {
         let n = 0;
-        (Object.keys(this._dataStore)).forEach((key) => {
+        (Object.keys(this._dataStore)).forEach((key:string) => {
             n++;
         })
         return n;
     }
 
     clear() {
-        (Object.keys(this._dataStore)).forEach((key) => {
+        (Object.keys(this._dataStore)).forEach((key:string) => {
             delete this._dataStore[key];
         })
     }
