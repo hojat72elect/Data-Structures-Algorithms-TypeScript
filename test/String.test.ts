0438a4feab929e044371939ab0a68f1dc0bf80ab
hojat <hojat72elect@gmail.com>
2023-06-06 13:39:08 -0400

    migrated string tests to TS

---
+++ test/String.test.ts
@@ -0,0 +1,12 @@
+describe('Stack unit tests', () => {
+
+    test('splitting a string into an array; letter by letter', () => {
+        const myMotto = "The life is what it is, not what it should be.";
+        expect(myMotto.split("").length).toBe(46);
+    })
+
+    test('splitting a string into an array; word by word', () => {
+        const myMotto = "The life is what it is, not what it should be.";
+        expect(myMotto.split(" ").length).toBe(11);
+    })
+});
