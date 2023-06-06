31759fc5dbb3f1dbab28444e363e86dc797552a1
hojat <hojat72elect@gmail.com>
2023-06-06 13:13:33 -0400

    Fully migrated Stack from js to ts

---
+++ src/ts/StackTS.ts
@@ -0,0 +1,42 @@
+export class StackTS {
+    private dataSource: any[];
+    length: number;
+
+    constructor() {
+        this.dataSource = [];
+        this.length = 0;
+    }
+
+    push(value: any) {
+        this.dataSource.push(value);
+        this.length++;
+    }
+
+    peek(): any {
+        if (this.length === 0) {
+            return null;
+        }
+        return this.dataSource[this.length - 1];
+    }
+
+    pop(): any {
+        if (this.length === 0) {
+            throw new Error("The stack is empty!!!");
+        }
+        this.length--;
+        return this.dataSource.pop();
+    }
+
+    clear() {
+        this.dataSource = [];
+        this.length = 0;
+    }
+
+    toString() {
+        let stringRepresentation = "";
+        [...this.dataSource].reverse().map((stackElement: any) => {
+            stringRepresentation += ` ${stackElement} -> `;
+        });
+        return stringRepresentation
+    }
+}
