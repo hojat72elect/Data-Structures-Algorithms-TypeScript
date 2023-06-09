e61efb69e2bbfd9e508dbfbc6a248752f52e1e7c
hojat <hojat72elect@gmail.com>
2023-06-09 16:25:28 -0400

    tests on built-in Set

--- test/Set.test.ts
+++ test/Set.test.ts
@@ -25,17 +25,21 @@ describe('Normal behavior of a Set', () => {
         // todo: Jest doesn't allow me to define a generic type for me Set in here.
         const sut = new Set();
 
-        sut.add({"Hojat": 29});
-        sut.add({"Katie": 20});
-        sut.add({"Hesam": 20});
-        sut.add({"Hamed": 34});
-        sut.add({"Zhe": 32});
-        sut.add({"Dad": 67});
-        sut.add({"Mom": 53});
+        sut.add({name: "Hojat", age: 29});
+        sut.add({name: "Katie", age: 20});
+        sut.add({name: "Hesam", age: 20});
+        sut.add({name: "Hamed", age: 34});
+        sut.add({name: "Zhe", age: 32});
+        sut.add({name: "Dad", age: 67});
+        sut.add({name: "Mom", age: 53});
 
-        // sut.forEach((person)=>{
-        //
-        // })
+        let stringRepresentation = ""
+        sut.forEach((person) => {
+            // @ts-ignore
+            stringRepresentation += `${person.name} is ${person.age} years old--`;
+        })
+
+        expect(stringRepresentation).toStrictEqual("Hojat is 29 years old--Katie is 20 years old--Hesam is 20 years old--Hamed is 34 years old--Zhe is 32 years old--Dad is 67 years old--Mom is 53 years old--")
     })
 
 });
