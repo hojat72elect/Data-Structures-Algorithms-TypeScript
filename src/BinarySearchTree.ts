022845e6118b82bc6da444ca90ff5be35a4cd1e8
hojat <hojat72elect@gmail.com>
2023-06-26 21:35:01 -0400

    new binary search tree

--- src/BinarySearchTree.ts
+++ src/BinarySearchTree.ts
@@ -1,7 +1,56 @@
 /**
  * This is the specific node used for creating a Binary Search Tree.
  */
-export class BinarySearchTreeNode<T>{
-    value:T;
+export class Node<T> {
+    value: T;
+    leftChild?: Node<T>;
+    rightChild?: Node<T>;
+
+    constructor(value: T) {
+        this.value = value;
+    }
+
+    toString(): string {
+        return this.value.toString();
+    }
+}
+
+export class BinarySearchTree<T> {
+    root: Node<T>;
+
+    constructor(root: Node<T>) {
+        this.root = root;
+    }
+
+    /**
+     * Inserts the provided value to the tree.
+     */
+    insert(data: T) {
+        let n = new Node(data);
+        if (this.root == null) {
+            this.root = n;
+            return;
+        }
+
+        let current = this.root;
+        let parent;
+        while (true) {
+            parent = current;
+            if (data < current.value) {
+                current = current.leftChild;
+                if (current == null) {
+                    parent.leftChild = n;
+                    break;
+                }
+            } else {
+                current = current.rightChild;
+                if (current == null) {
+                    parent.rightChild = n;
+                    break;
+                }
+            }
+        }
+    }
+
 
 }
