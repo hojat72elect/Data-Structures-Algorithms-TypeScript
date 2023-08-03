02f1431f6809e9c24c05f2a0224bfbcfb303f7a6
hojat <hojat72elect@gmail.com>
2023-08-02 22:46:26 -0400

    docs

--- src/lodash/internal/GetTag.ts
+++ src/lodash/internal/GetTag.ts
@@ -3,8 +3,16 @@
  * it can be applied to all the possible values and will print out
  * an understandable string for us. But "typeof" cannot recognize null.
  *
+ * This function correctly works on instances of type number,
+ * string, boolean, null, undefined, and Array; and can print
+ * out distinguished outputs for all of them.
+ *
+ * But it cannot distinguish between a class instance and normal
+ * JS object.
+ *
+ *
  * @param value {any} => input of type any|null|undefined
- * @returns {string} the toStringTag of that value.
+ * @returns {string} the correct tag of that value.
  */
 export function getTag(value: any): string {
 
